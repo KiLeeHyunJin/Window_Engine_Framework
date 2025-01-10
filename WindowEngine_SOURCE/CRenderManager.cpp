306694bc9f7410126a9d241f07650baf3e71dacf
DeskTop_LHJ <kistudentmeta5@gmail.com>
2025-01-11 01:17:52 +0900

    feat : RenderManager

--- WindowEngine_SOURCE/CRenderManager.cpp
+++ WindowEngine_SOURCE/CRenderManager.cpp
@@ -2,5 +2,28 @@
 
 namespace Framework
 {
-
+	void CRenderManager::Initialize(HWND hWnd, int width, int height, int xPos, int yPos, DWORD winStyle, bool menu)
+	{
+	}
+	void CRenderManager::Release()
+	{
+	}
+	void CRenderManager::Render()
+	{
+	}
+	void CRenderManager::BeginDraw()
+	{
+	}
+	void CRenderManager::EndDraw()
+	{
+	}
+	void CRenderManager::AdjustWindow(HWND hWnd, int width, int height, int xPos, int yPos, DWORD winStyle, bool menu)
+	{
+	}
+	void CRenderManager::ChangeScreenSize(bool maximumScale)
+	{
+	}
+	void CRenderManager::CreateBackBuffer(int width, int height)
+	{
+	}
 }
--- WindowEngine_SOURCE/CRenderManager.h
+++ WindowEngine_SOURCE/CRenderManager.h
@@ -1,14 +1,44 @@
 #pragma once
+#include "CommonInclude.h"
 
 namespace Framework
 {
+	class CApplication;
 
 	class CRenderManager
 	{
 	public:
+		static void Initialize(HWND hWnd, int  width, int height, int xPos, int yPos, DWORD winStyle, bool menu);
+		static void Release();
 
+		static void Render();
+
+		static void BeginDraw();
+		static void EndDraw();
+
+		static void AdjustWindow(HWND hWnd, int width, int height, int xPos, int yPos, DWORD winStyle, bool menu);
+		static void ChangeScreenSize(bool maximumScale);
+
+		static void CreateBackBuffer(int width, int height);
+
+		friend CApplication;
 	private:
+		static HDC GetHDC() { return m_hDC; }
 
+		static HWND m_hWnd;
+		static HDC m_hDC;
+
+		static HDC m_BackHDC;
+		static HBITMAP m_BmpBuffer;
+
+		static int m_iWindowWidth;	//창모드 사이즈
+		static int m_iWindowHeight;
+
+		static int m_iScreenWidth; //화면 해상도
+		static int m_iScreenHeight;
+
+		static int m_iCurrentBufferBitmapWidth;
+		static int m_iCurrentBufferBitmapHeight;
 	};
 
 }
