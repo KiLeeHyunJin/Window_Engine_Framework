c6c30b81476c269a1f2c32f45ea4f79b0bbb68b4
DeskTop_LHJ <kistudentmeta5@gmail.com>
2025-03-07 01:32:00 +0900

    edit : TimeCalc  - deltaTime 계산식 분리

--- WindowEngine_SOURCE/CTimeManager.cpp
+++ WindowEngine_SOURCE/CTimeManager.cpp
@@ -2,17 +2,13 @@
 
 namespace Framework
 {
-	LARGE_INTEGER	CTimeManager::m_liCpuFrequency		= {};
-	LARGE_INTEGER	CTimeManager::m_liPrevFrequency		= {};
-	LARGE_INTEGER	CTimeManager::m_liCurrentFrequency	= {};
-
-	ULONGLONG		CTimeManager::m_fDeltaTick			= 0;
-	ULONGLONG		CTimeManager::m_fPrevTick			= 0;
-
 	float			CTimeManager::m_fDeltaTime			= 0;
 	UINT			CTimeManager::m_uiFPS				= 0;
-	std::chrono::high_resolution_clock::time_point CTimeManager::curTime	= {};
-	std::chrono::high_resolution_clock::time_point CTimeManager::prevTime	= {};
+	CTimeManager::eTimeType CTimeManager::timeType		= CTimeManager::eTimeType::Chrono;
+	CTimeManager::FunctionPtr CTimeManager::functions[3]= { nullptr};
+
+	LARGE_INTEGER	CTimeManager::m_liCpuFrequency		= {};
+	LARGE_INTEGER	CTimeManager::m_liPrevFrequency		= {};
 
 	CTimeManager::CTimeManager()		{	}
 	
@@ -22,51 +18,25 @@ namespace Framework
 	{
 		QueryPerformanceFrequency(&m_liCpuFrequency);
 		QueryPerformanceCounter(&m_liPrevFrequency);
+
+		functions[(int)eTimeType::Tick]			= CTimeManager::TimeTick;
+		functions[(int)eTimeType::Performance]	= CTimeManager::TimePerformance;
+		functions[(int)eTimeType::Chrono]		= CTimeManager::TimeChrono;
 	}
 
 	void CTimeManager::Tick()
 	{
-
-#pragma region NoUseDeltaTimeCalc
-		// //Performance Time //CPU 클럭 주파수를 기반, 정확도 - 매우 높음, 일관성 - 일부 멀티 코어 환경에서 문제 발생 가능
-		//{
-		//	QueryPerformanceCounter(&m_liCurrentFrequency);
-		//	float dirrentFrequency = static_cast<float>(m_liCurrentFrequency.QuadPart - m_liPrevFrequency.QuadPart);
-
-		//	m_fDeltaTime = dirrentFrequency / static_cast<float>(m_liCpuFrequency.QuadPart);
-
-		//	m_liPrevFrequency.QuadPart = m_liCurrentFrequency.QuadPart;
-		//}
-		// //Tick Time //OS 타이머 기반 정확도 - 낮음, 
-		//{
-		//	const ULONGLONG currentTick = GetTickCount64();
-		//	m_fDeltaTick = currentTick - m_fPrevTick;
-		//	m_fPrevTick = currentTick;
-		//}
-
-#pragma endregion
-
+		functions[(UINT)timeType]();
 		
-		//Chrono Time //C++11 이후 표준 라이브러리 정확도 ㅔ 매우 높음, 일관성 - 운영체제에 종속되어 있는 않는 기능(독립적)
+		//지연에 의한 순간이동 현상을 억제
+		if (m_fDeltaTime > 0.1f)
 		{
-			curTime = std::chrono::high_resolution_clock::now();
-
-			std::chrono::duration<float> elapsed = curTime - prevTime; // 이전 프레임이랑 차이 시간 계산
-			prevTime = curTime;
-
-			m_fDeltaTime = std::clamp(elapsed.count(), 0.0f, 0.1f);
-
-			//지연에 의한 순간이동 현상을 억제
-			if (m_fDeltaTime > 0.1f)
-			{
-				m_fDeltaTime = 0.1f;
-			}
+			m_fDeltaTime = 0.1f;
 		}
-
 #pragma region FPS
 
 		static float stackDeltaTime = 0;
-		static int tickCount = 0;
+		static UINT tickCount = 0;
 
 		stackDeltaTime += m_fDeltaTime;
 
@@ -81,8 +51,6 @@ namespace Framework
 			tickCount += 1;
 		}
 #pragma endregion
-
-
 	}
 
 	void CTimeManager::Render(HDC hdc)
@@ -90,13 +58,57 @@ namespace Framework
 		//static float time = 0;
 		
 		//time += m_fDeltaTime;
-		float fps = 1.0f / m_fDeltaTime;
+		//float fps = 1.0f / m_fDeltaTime;
 
 		wchar_t str[50] = L"";
-		swprintf_s(str, 50, L"FPS : %d", (int)fps);
+		swprintf_s(str, 50, L"FPS : %d", (int)m_uiFPS);
 		int len = (int)wcsnlen_s(str, 50);
 
 		TextOut(hdc, 0, 0, str, len);
+	}
+
+	/// <summary>
+	/// OS 타이머 기반 정확도 - 낮음, 
+	/// </summary>
+	void CTimeManager::TimeTick()
+	{
+		static ULONGLONG m_fDeltaTick	= 0;
+		static ULONGLONG m_fPrevTick	= 0;
+		const  ULONGLONG currentTick	= GetTickCount64();
+
+		m_fDeltaTick = currentTick - m_fPrevTick;
+		m_fDeltaTime = m_fDeltaTick * 0.001f;
+		m_fPrevTick = currentTick;
+	}
+
+	/// <summary>
+	/// CPU 클럭 주파수를 기반, 정확도 - 매우 높음, 일관성 - 일부 멀티 코어 환경에서 문제 발생 가능
+	/// </summary>
+	void CTimeManager::TimePerformance()
+	{
+		LARGE_INTEGER	m_liCurrentFrequency = {};
+		QueryPerformanceCounter(&m_liCurrentFrequency);
+
+		float dirrentFrequency = static_cast<float>(m_liCurrentFrequency.QuadPart - m_liPrevFrequency.QuadPart);
+		m_fDeltaTime = dirrentFrequency / static_cast<float>(m_liCpuFrequency.QuadPart);
+		
+		m_liPrevFrequency.QuadPart = m_liCurrentFrequency.QuadPart;
+	}
+
+	/// <summary>
+	/// C++11 이후 표준 라이브러리 정확도 - 매우 높음, 일관성 - 운영체제에 종속되어 있는 않는 기능(독립적)
+	/// </summary>
+	void CTimeManager::TimeChrono()
+	{
+		static std::chrono::high_resolution_clock::time_point curTime	= {};
+		static std::chrono::high_resolution_clock::time_point prevTime	= {};
+
+		curTime = std::chrono::high_resolution_clock::now();
+
+		std::chrono::duration<float> elapsed = curTime - prevTime; // 이전 프레임이랑 차이 시간 계산
+		prevTime = curTime;
+
+		m_fDeltaTime = std::clamp(elapsed.count(), 0.0f, 0.1f);
 	}
 
 
--- WindowEngine_SOURCE/CTimeManager.h
+++ WindowEngine_SOURCE/CTimeManager.h
@@ -9,12 +9,19 @@ namespace Framework
 	class CTimeManager
 	{
 	public:
+		enum class eTimeType
+		{
+			Tick,
+			Performance,
+			Chrono
+		};
 		CTimeManager();
 		virtual ~CTimeManager();
 
-		__forceinline static float DeltaTime()		{ return m_fDeltaTime; }
-		__forceinline static float FPS()			{ return m_uiFPS; }
-		__forceinline static ULONGLONG TickTime()	{ return m_fDeltaTick; }
+		__forceinline static void SetTimeType(eTimeType type)		{ timeType = type; }
+		__forceinline static float DeltaTime()						{ return m_fDeltaTime; }
+		__forceinline static UINT FPS()								{ return m_uiFPS; }
+		//__forceinline static ULONGLONG TickTime()					{ return m_fDeltaTime; }
 
 		friend CApplication;
 		friend CRenderManager;
@@ -23,21 +30,20 @@ namespace Framework
 		static void Tick();
 		static void Render(HDC hdc);
 
+		static void TimeTick();
+		static void TimePerformance();
+		static void TimeChrono();
+
 		static float m_fDeltaTime;
 		static UINT m_uiFPS;
+		static eTimeType timeType;
+
+		using FunctionPtr = void(*)();
+		static FunctionPtr functions[3];
 
 		/// CPU Preformance
 		static LARGE_INTEGER m_liCpuFrequency;
 		static LARGE_INTEGER m_liPrevFrequency;
-		static LARGE_INTEGER m_liCurrentFrequency;
-
-		/// Chrono Time
-		static std::chrono::high_resolution_clock::time_point curTime;
-		static std::chrono::high_resolution_clock::time_point prevTime;
-
-		///OS Tick Count
-		static ULONGLONG m_fPrevTick;
-		static ULONGLONG m_fDeltaTick;
 	};
 	using TIME = CTimeManager;
 //#define TIME CTimeManager
