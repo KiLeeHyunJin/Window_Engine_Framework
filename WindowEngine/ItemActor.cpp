823db429a0c5225df47b2f0bb5b5d6fd6e4a8965
DeskTop_LHJ <kistudentmeta5@gmail.com>
2025-05-04 00:06:15 +0900

    feat : Start ItemActor

---
+++ WindowEngine/ItemActor.cpp
@@ -0,0 +1,36 @@
+#include "ItemActor.h"
+
+
+namespace Framework
+{
+	ItemActor::ItemActor()
+	{
+	}
+	ItemActor::~ItemActor()
+	{
+	}
+	void ItemActor::Initialize()
+	{
+	}
+	void ItemActor::BeginPlay()
+	{
+	}
+	void ItemActor::Release()
+	{
+	}
+	bool ItemActor::Tick()
+	{
+		return false;
+	}
+	bool ItemActor::LastTick()
+	{
+		return false;
+	}
+	void ItemActor::FixedTick()
+	{
+	}
+	bool ItemActor::Render(HDC hdc) const
+	{
+		return false;
+	}
+}
---
+++ WindowEngine/ItemActor.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "..//WindowEngine_SOURCE//CSpriteActor.h"
+
+
+namespace Framework
+{
+
+    class ItemActor :
+        public CSpriteActor
+    {
+    public:
+		ItemActor();
+		virtual ~ItemActor();
+
+		void Initialize()			override;
+		void BeginPlay()			override;
+		void Release()				override;
+		bool Tick()					override;
+		bool LastTick()				override;
+		void FixedTick()			override;
+		bool Render(HDC hdc) const	override;
+    private:
+
+    };
+}
+
+
